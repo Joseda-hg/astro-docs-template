51ad5e70034a6e4b81e4ddd7244e52de279c35f7
Jose Hernandez <71994263+Joseda-hg@users.noreply.github.com>
2025-01-16 11:03:46 -0300

    Add default locale configuration and migrate "Flujo de Pedidos" guide to reference section

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,25 +9,37 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Docs',
+			// Establece el inglés como el idioma predeterminado para este sitio.
+			defaultLocale: 'root',
+			locales: {
+				// Documentación en inglés en `src/content/docs/en/`
+				en: {
+					label: 'English',
+				},
+				root: {
+					label: 'Español',
+					lang: 'es', // lang es obligatorio para los locales raíz
+				  },
+			},
 			logo: {
 				src: './src/assets/Logo-PG.png',
-			  },
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', slug: 'guides/example' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
 		}),
 	],
 });
